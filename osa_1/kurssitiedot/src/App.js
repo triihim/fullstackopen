bb0c2dd41f2397fd9cba1d5dfa0977e43b0b4192
Tatu Riihimäki <tatu.riihimaki@gmail.com>
2021-03-17 19:51:00 +0200

    1.1 ja 1.2 kurssitiedot tehtävät

---
+++ osa_1/kurssitiedot/src/App.js
@@ -0,0 +1,68 @@
+/*
+  HUOM! Tehtävänannossa käskettiin poistamaan ylimääräiset tiedostot:
+  App.css, App.test.js, logo.svg, reportWebVitals.js, setupTests.js,
+  mutta reportWebVitalis.js:n poiston jälkeen ohjelma ei enää toimi.
+  Tästä syystä edelleen tiedostoissa mukana.
+*/
+
+import React from 'react'
+
+const Header = (props) => {
+  return (
+    <header>
+      <h1>{props.course}</h1>
+    </header>
+  );
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.name} {props.exercises}
+    </p>
+  );
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part name={props.part1} exercises={props.exercises1} />
+      <Part name={props.part2} exercises={props.exercises2} />
+      <Part name={props.part3} exercises={props.exercises3} />
+    </div>
+  );
+}
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises {props.count}
+    </p>
+  );
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content 
+        part1={part1}
+        part2={part2}
+        part3={part3}
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3} />
+      <Total count={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+export default App
