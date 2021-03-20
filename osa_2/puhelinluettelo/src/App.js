c930e2dfdf581c0cc9e40a3dadc1a83cbd8a0925
Tatu Riihimäki <tatu.riihimaki@gmail.com>
2021-03-20 12:15:54 +0200

    Up to 2.10: puhelinluettelo step5

---
+++ osa_2/puhelinluettelo/src/App.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react'
+
+const Filter = ({value, handleChange}) => {
+  return (
+    <div>
+      filter: <input value={value} onChange={handleChange} />
+    </div>
+  );
+}
+
+const PersonForm = (props) => {
+  const name = props.name;
+  const number = props.number;
+  const handleNameChange = props.handleNameChange;
+  const handleNumberChange = props.handleNumberChange;
+  const handleSubmit = props.handleSubmit;
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        name: <input value={name} onChange={handleNameChange} />
+      </div>
+      <div>
+        number: <input value={number} onChange={handleNumberChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+}
+
+const Persons = ({persons}) => {
+  return (
+    <div>
+      {persons.map(p => <p key={p.name}>{p.name} {p.number}</p>)}
+    </div>
+  );
+}
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]) 
+
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('');
+  const [ filter, setFilter ] = useState('');
+
+  const filteredPersons = persons.filter(p => p.name.toLowerCase().includes(filter.toLowerCase()));
+
+  const addContact = (event) => {
+    event.preventDefault();
+    const nameAlreadyExists = () => !!persons.find(p => p.name.toLowerCase() === newName.toLowerCase());
+    const nameIsProvided = () => !!newName && newName.length > 0;
+    const numberIsProvided = () => !!newNumber && newNumber.length > 6; // Mock constraint.
+
+    if(nameAlreadyExists()) {
+      alert(`${newName} is already added to the phonebook`);
+    } else if(nameIsProvided() && numberIsProvided()) {
+      setPersons(persons.concat({name: newName, number: newNumber}));
+    } else {
+      alert("Please provide both the name and the number");
+    }
+  }
+
+  const handleNameChange = event => setNewName(event.target.value);
+  const handleNumberChange = event => setNewNumber(event.target.value);
+  const handleFilterChange = event => setFilter(event.target.value);
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+        <Filter value={filter} handleChange={handleFilterChange} />
+      <h2>Add new contact</h2>
+        <PersonForm 
+          name={newName} 
+          number={newNumber} 
+          handleNameChange={handleNameChange}
+          handleNumberChange={handleNumberChange}
+          handleSubmit={addContact} />
+      <h2>Numbers</h2>
+        <Persons persons={filteredPersons} />
+    </div>
+  )
+
+}
+
+export default App
