c5b2f34a89dcc4b7fe9b5be5723e49ce7b8bfe78
Tatu Riihimäki <tatu.riihimaki@gmail.com>
2021-03-20 19:48:16 +0200

    Up to 2.18*

--- osa_2/puhelinluettelo/src/App.js
+++ osa_2/puhelinluettelo/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import contactService from "./services/contactService";
 
 const Filter = ({value, handleChange}) => {
   return (
@@ -9,7 +9,7 @@ const Filter = ({value, handleChange}) => {
   );
 }
 
-const PersonForm = (props) => {
+const ContactForm = (props) => {
   const name = props.name;
   const number = props.number;
   const handleNameChange = props.handleNameChange;
@@ -30,61 +30,98 @@ const PersonForm = (props) => {
   );
 }
 
-const Persons = ({persons}) => {
+const ContactList = ({contacts, onDelete}) => {
   return (
     <div>
-      {persons.map(p => <p key={p.name}>{p.name} {p.number}</p>)}
+      {contacts.map(c => {
+        return (
+          <div key={c.id}>
+            <span>{c.name} {c.number}</span>
+            <button onClick={() => onDelete(c.id)}>Delete</button>
+          </div>
+        );
+      })}
     </div>
   );
 }
 
 const App = () => {
-  const [ persons, setPersons] = useState([]) ;
-
-  useEffect(() => {
-    axios.get("http://localhost:3001/persons")
-      .then(res => setPersons(persons.concat(res.data)))
-      .catch(console.error);
-  }, []);
-
+  const [ contacts, setContacts] = useState([]);
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('');
   const [ filter, setFilter ] = useState('');
 
-  const filteredPersons = persons && persons.filter(p => p.name.toLowerCase().includes(filter.toLowerCase()));
+  const nameAlreadyExists = () => !!contacts.find(p => p.name.toLowerCase() === newName.toLowerCase());
+  const nameIsProvided = () => !!newName && newName.length > 0;
+  const numberIsValid = () => !!newNumber && newNumber.length > 6; // Mock constraint.
+  const sortContactsByName = (c1, c2) => c2.name.toLowerCase() < c1.name.toLowerCase() ? 1 : -1;
 
-  const addContact = (event) => {
-    event.preventDefault();
-    const nameAlreadyExists = () => !!persons.find(p => p.name.toLowerCase() === newName.toLowerCase());
-    const nameIsProvided = () => !!newName && newName.length > 0;
-    const numberIsProvided = () => !!newNumber && newNumber.length > 6; // Mock constraint.
+  useEffect(() => {
+    contactService.getContacts()
+      .then(fetchedContacts => setContacts(c => c.concat(fetchedContacts)))
+      .catch(console.error);
+  }, []);
 
-    if(nameAlreadyExists()) {
-      alert(`${newName} is already added to the phonebook`);
-    } else if(nameIsProvided() && numberIsProvided()) {
-      setPersons(persons.concat({name: newName, number: newNumber}));
+  const filteredContacts = contacts && contacts.filter(c => c.name.toLowerCase().includes(filter.toLowerCase())).sort(sortContactsByName);
+
+  const createContract = () => {
+    contactService.createContact({name: newName, number: newNumber})
+      .then(createdContact => {
+        setContacts(contacts.concat(createdContact))
+        setNewName("");
+        setNewNumber("");
+      })
+      .catch(console.error);
+  }
+
+  const deleteContact = id => {
+    if(window.confirm(`Delete ${contacts.find(c => c.id === id).name}?`)) {
+      contactService.deleteContact(id)
+        .then(() => setContacts(contacts.filter(c => c.id !== id)))
+        .catch(console.error)
+    }
+  }
+
+  const updateContactNumber = () => {
+    const existingContact = contacts.find(c => c.name.toLowerCase() === newName.toLowerCase());
+    if(numberIsValid()) {
+      contactService.updateNumber(existingContact.id, {name: existingContact.name, number: newNumber})
+        .then(updatedContact => setContacts(contacts.filter(c => c.id !== existingContact.id).concat(updatedContact)))
+        .catch(console.error);
     } else {
-      alert("Please provide both the name and the number");
+      alert("Provide a proper number");
+    }
+  }
+
+  const attemptContactCreation = () => {
+    if(nameAlreadyExists() && window.confirm(`${newName} is already added to the phonebook. Update existing number?`)) {
+      updateContactNumber();
+    } else if(nameIsProvided() && numberIsValid()) {
+      createContract();
+    } else {
+      alert("Please provide both the name and the number. Also, ensure the number is long enough");
     }
   }
 
   const handleNameChange = event => setNewName(event.target.value);
   const handleNumberChange = event => setNewNumber(event.target.value);
   const handleFilterChange = event => setFilter(event.target.value);
+  const handleContactDelete = contactId => deleteContact(contactId);
+  const handleContactFormSubmit = event => event.preventDefault() || attemptContactCreation();
 
   return (
     <div>
       <h2>Phonebook</h2>
         <Filter value={filter} handleChange={handleFilterChange} />
       <h2>Add new contact</h2>
-        <PersonForm 
+        <ContactForm 
           name={newName} 
           number={newNumber} 
           handleNameChange={handleNameChange}
           handleNumberChange={handleNumberChange}
-          handleSubmit={addContact} />
+          handleSubmit={handleContactFormSubmit} />
       <h2>Numbers</h2>
-        <Persons persons={filteredPersons} />
+        <ContactList contacts={filteredContacts} onDelete={handleContactDelete} />
     </div>
   )
 
